fc905c2e7dde93ebb602cca03464fbb0855868d7
ThiagoAM <thiago_andersonmm@hotmail.com>
2017-05-12 08:51:42 -0300

    upload 3

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,10 +64,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         // Presents the image picker:
         present(imagePickerController, animated: true, completion: nil)
     }
-    
-    @IBAction func setDefaultTextLabel(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
 }
 
